c835ec3a15c53e1fe19c25bbced7da0ac6b22b9a
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-16 23:41:45 -0400

    initial commit functional programmatic view controller

---
+++ Happiness/ViewController/NavigationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationViewController.swift
+//  Happiness
+//
+//  Created by Michael Vilabrera on 5/16/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController
+{
+    
+}
