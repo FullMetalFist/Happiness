e5eeb2bb1e3bc7e50de7e478831450a79dd17bf7
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-18 11:40:30 -0400

    successful vfl constraints added

--- Happiness/ViewController/HappinessViewController.swift
+++ Happiness/ViewController/HappinessViewController.swift
@@ -22,8 +22,8 @@ class HappinessViewController: UIViewController
     func makeLayout() {
         face = UIView()
         face.setTranslatesAutoresizingMaskIntoConstraints(false)
-//        face.backgroundColor = UIColor(red: 0.3, green: 0.4, blue: 0.1, alpha: 1)
-        face.backgroundColor = UIColor.blueColor()
+        face.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 1.0, alpha: 1)
+//        face.backgroundColor = UIColor.blueColor()
         view.addSubview(face)
         
         //-------------------- constraints
@@ -32,12 +32,17 @@ class HappinessViewController: UIViewController
         let viewsDictionary = ["face":face]
         
         // constraint strings
-        let face_H: String! = "H:[face(300)]"
-        let face_V: String! = "V:[face(500)]"
+        let face_H: String! = "H:[face]"
+        let face_V: String! = "V:[face]"
         // sizing constraints
         let face_constraint_H: Array = NSLayoutConstraint.constraintsWithVisualFormat(face_H, options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
         let face_constraint_V: Array = NSLayoutConstraint.constraintsWithVisualFormat(face_V, options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
         face.addConstraints(face_constraint_H)
         face.addConstraints(face_constraint_V)
+        
+        // necessary to add constraints to superview
+        // use the pipe here
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[face]|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[face]|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary))
     }
 }
